60845ae1027522df201cb5004f9484d5e995d7cc
karlpederolesen <96655854+karlpederolesen@users.noreply.github.com>
2022-02-12 19:36:32 +0100

    Opdater pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    if (kør == true) {
+        kør = false
+    } else {
+        kør = true
+    }
+    if (kør == true) {
+        RingbitCar.forward()
+    } else {
+        RingbitCar.brake()
+    }
+})
+let kør = false
+kør = false
+let strip = neopixel.create(DigitalPin.P0, 2, NeoPixelMode.RGB)
+RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P2)
 basic.forever(function () {
-	
+    strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+    basic.showString("BA")
+    strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    basic.showString("BU")
 })
